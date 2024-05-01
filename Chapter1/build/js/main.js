ba06fe36e05c6675a32f92237bf1c73ee44a2561
AndreMuderhwa <safariandre66@gmail.com>
2024-05-01 13:38:31 +0200

    first chapter

---
+++ Chapter1/build/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+// let username="Lydia"
+// console.log(username)
+// let a : number=12
+// let b : number=6
+// let c : number=2
+// console.log(a/b)
+// console.log(c*b)
+//ABOUT TYPES
+//IMPLICIT
+let myName = "Gla";
+//EXPLCIT
+let names;
+let meaningOflife;
+let isLoading;
+let album; //peut recevoir les donnees de tout types(number, string, boolean, arrays,objects)
+names = "GLA";
+meaningOflife = 129;
+isLoading = false;
---
+++ Chapter1/src/main.ts
@@ -0,0 +1,24 @@
+// let username="Lydia"
+// console.log(username)
+
+// let a : number=12
+// let b : number=6
+// let c : number=2
+
+// console.log(a/b)
+// console.log(c*b)
+
+//ABOUT TYPES
+//IMPLICIT
+let myName="Gla"
+//EXPLCIT
+let names : string;
+let meaningOflife:number;
+let isLoading: boolean;
+let album : any; //peut recevoir les donnees de tout types(number, string, boolean, arrays,objects)
+
+names="GLA"
+meaningOflife=129
+isLoading=false
+
+
