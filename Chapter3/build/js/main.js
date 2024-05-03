41c7d8ee5329379a9ce418969b9428e94039365c
AndreMuderhwa <safariandre66@gmail.com>
2024-05-03 12:47:56 +0200

    finish functions

--- Chapter3/build/js/main.js
+++ Chapter3/build/js/main.js
@@ -11,7 +11,7 @@ const logMessage = (message) => {
 };
 logMessage("Hello");
 // logMessage(436)
-logMessage(sumNumbers(12, 18));
+// logMessage(sumNumbers(12,18))
 let substract = function (a, b) {
     return a - b;
 };
@@ -21,7 +21,7 @@ let substract = function (a, b) {
 let multiply = function (a, b) {
     return a * b;
 };
-logMessage(multiply(4, 3));
+// logMessage(multiply(4,3))
 //optional parameters
 const addAll = (a, b, c) => {
     if (typeof c !== 'undefined') {
@@ -33,6 +33,35 @@ const addAll = (a, b, c) => {
 const sumAll = (a = 12, b, c) => {
     return a + b + c;
 };
-logMessage(addAll(3, 3));
-logMessage(sumAll(undefined, 2, 5));
-//Rest paramters
+// logMessage(addAll(3,3))
+// logMessage(sumAll(undefined,2,5))
+//Rest paramters =(...nums: number[])
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => (prev + curr));
+};
+const totalSurcharge = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => (prev + curr));
+};
+// logMessage(total(10,4,5))
+logMessage(totalSurcharge(10, 4, 5));
+//Never type is used when inside the function we have infinite loop
+const createError = (error) => {
+    throw new Error(error);
+};
+const infiniteSayHello = () => {
+    while (true) {
+        console.log("Say Hello");
+    }
+};
+//custom type guard
+const isNumber = (value) => {
+    return typeof value === 'number' ? true : false;
+};
+//use of the never type
+const numberOrString = (value) => {
+    if (typeof value === 'string')
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    return createError("This error can't be happen");
+};
--- Chapter3/src/main.ts
+++ Chapter3/src/main.ts
@@ -25,7 +25,7 @@ const logMessage =(message: any): void=>{
 
 logMessage("Hello")
 // logMessage(436)
-logMessage(sumNumbers(12,18))
+// logMessage(sumNumbers(12,18))
 
 let substract = function(a: number, b: number): number{
     return a-b
@@ -39,7 +39,7 @@ let multiply : mathFunction =function(a,b){
     return a*b
 }
 
-logMessage(multiply(4,3))
+// logMessage(multiply(4,3))
 
 //optional parameters
 const addAll=(a: number, b: number, c?:number): number=>{
@@ -52,6 +52,40 @@ const addAll=(a: number, b: number, c?:number): number=>{
 const sumAll=(a: number=12, b: number, c:number): number=>{
      return a+b+c
  }
-logMessage(addAll(3,3))
-logMessage(sumAll(undefined,2,5))
-//Rest paramters
+// logMessage(addAll(3,3))
+// logMessage(sumAll(undefined,2,5))
+//Rest paramters =(...nums: number[])
+
+const total =(...nums:number[]):number=>{
+   return nums.reduce((prev,curr)=>(prev+curr))
+}
+const totalSurcharge =(a: number, ...nums:number[]):number=>{
+    return a + nums.reduce((prev,curr)=>(prev+curr))
+ }
+// logMessage(total(10,4,5))
+logMessage(totalSurcharge(10,4,5))
+
+//Never type is used when inside the function we have infinite loop
+
+const createError=(error : string): never=>{
+    throw new Error(error)
+}
+
+const infiniteSayHello=()=>{
+    while(true){
+        console.log("Say Hello")
+    }
+}
+
+//custom type guard
+const isNumber=(value : any):boolean=>{
+    return typeof value === 'number' ? true : false
+}
+
+
+//use of the never type
+const numberOrString=(value : string | number): string=>{
+    if (typeof value ==='string')  return 'string'
+    if(isNumber(value)) return 'number'
+    return createError("This error can't be happen")
+}
