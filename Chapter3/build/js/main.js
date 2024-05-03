684d009c6c1efc4961a289296c0206c76aaea3e4
AndreMuderhwa <safariandre66@gmail.com>
2024-05-03 11:36:00 +0200

    functions in ts

--- Chapter3/build/js/main.js
+++ Chapter3/build/js/main.js
@@ -1,1 +1,38 @@
 "use strict";
+//LITTERAL TYPES
+let myName;
+let useName;
+//functions
+const sumNumbers = (a, b) => {
+    return a + b;
+};
+const logMessage = (message) => {
+    console.log(message);
+};
+logMessage("Hello");
+// logMessage(436)
+logMessage(sumNumbers(12, 18));
+let substract = function (a, b) {
+    return a - b;
+};
+// interface mathFunction{
+//     (a: number, b: number): number
+// }
+let multiply = function (a, b) {
+    return a * b;
+};
+logMessage(multiply(4, 3));
+//optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+//default parameter value
+const sumAll = (a = 12, b, c) => {
+    return a + b + c;
+};
+logMessage(addAll(3, 3));
+logMessage(sumAll(undefined, 2, 5));
+//Rest paramters
--- Chapter3/src/main.ts
+++ Chapter3/src/main.ts
@@ -1,6 +1,57 @@
-interface Guitarist{
+type stringOrnumber = string | number
+
+type stringOrnumberArray= (string | number)[]
+
+type Guitarist ={
     name ? : string,
     active : boolean,
-    albums : (string | number)[]
+    albums : stringOrnumberArray
 
 }
+type UserId =stringOrnumber
+
+//LITTERAL TYPES
+let myName : "Glo"
+let useName : "Dave" | "Gray" | "Yellow"
+//functions
+
+const sumNumbers=(a: number, b: number):number =>{
+    return a+b
+}
+
+const logMessage =(message: any): void=>{
+    console.log(message)
+}
+
+logMessage("Hello")
+// logMessage(436)
+logMessage(sumNumbers(12,18))
+
+let substract = function(a: number, b: number): number{
+    return a-b
+}
+ type mathFunction =(a: number, b: number)=> number
+// interface mathFunction{
+//     (a: number, b: number): number
+// }
+
+let multiply : mathFunction =function(a,b){
+    return a*b
+}
+
+logMessage(multiply(4,3))
+
+//optional parameters
+const addAll=(a: number, b: number, c?:number): number=>{
+   if(typeof c !== 'undefined'){
+    return a+b+c
+   }
+   return a+b
+}
+//default parameter value
+const sumAll=(a: number=12, b: number, c:number): number=>{
+     return a+b+c
+ }
+logMessage(addAll(3,3))
+logMessage(sumAll(undefined,2,5))
+//Rest paramters
