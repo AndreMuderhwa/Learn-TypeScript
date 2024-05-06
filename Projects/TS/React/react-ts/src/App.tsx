9c4101cff1deb8bce3c22f94037108f728099d56
AndreMuderhwa <safariandre66@gmail.com>
2024-05-06 12:40:09 +0200

    some hooks of react with ts

--- Projects/TS/React/react-ts/src/App.tsx
+++ Projects/TS/React/react-ts/src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from "react"
-import Heading from "./components/Heading"
-import { Section } from "./components/Section"
-import Counter from "./components/Counter"
-import List from "./components/List"
+import { useCallback, useEffect, useState } from "react"
+// import Heading from "./components/Heading"
+// import { Section } from "./components/Section"
+// import Counter from "./components/Counter"
+// import List from "./components/List"
+interface User{
+    id: number,
+    username: string
+}
 
 function App() {
-  const [count, setCount]=useState<number>(1)
+  // const [count, setCount]=useState<number>(1)
+  const [count, setCount]=useState<number>(0)
+  const [users, setUsers]=useState<User[] | null>(null)
   
-  const title : string ="Hello World"
+  // const title : string ="Hello World"
+  useEffect(()=>{
+    console.log("mounted")
+
+    return()=>console.log("Unmounted")
+
+  },[users])
+  const addTwo = useCallback((): void=>setCount(prev =>prev+1), []);
   return (
     <div>
-      <Heading title={title}/>
+      {/* <Heading title={title}/>
       <Section title={"Another title"}>This is the children</Section>
       <Counter setCount={setCount}>{count}</Counter>
       <List items={['Coffe','Tacos','code']} render={(item: string)=><span className="gold">{item}</span>}/>
-     
+      */}
+      <h2>Count {count}</h2>
+      <button onClick={addTwo}>+</button>
     </div>
   )
 }
